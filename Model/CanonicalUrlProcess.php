3f2adc1a6a06ffed903993235832f7d5679f5bec
Vladymyr Hrivinskyi <volodymyr@hryvinskyi.com>
2023-08-18 20:23:45 +0300

    Update CanonicalUrlProcess.php

--- Model/CanonicalUrlProcess.php
+++ Model/CanonicalUrlProcess.php
@@ -9,15 +9,18 @@ namespace Hryvinskyi\SeoCanonicalAmastyShopByBrand\Model;
 use Amasty\ShopbyBrand\Model\ConfigProvider;
 use Hryvinskyi\SeoCanonicalFrontend\Model\AbstractCanonicalUrlProcess;
 use Magento\Framework\App\HttpRequestInterface;
+use Magento\Framework\UrlInterface;
 
 class CanonicalUrlProcess extends AbstractCanonicalUrlProcess
 {
     private ConfigProvider $configProvider;
+    private ?UrlInterface $url = null;
 
-    public function __construct(ConfigProvider $configProvider, array $actions = [])
+    public function __construct(ConfigProvider $configProvider, array $actions = [], UrlInterface $url = null)
     {
         parent::__construct($actions);
         $this->configProvider = $configProvider;
+        $this->url = $url ?: \Magento\Framework\App\ObjectManager::getInstance()->get(UrlInterface::class);
     }
 
     /**
@@ -50,7 +53,6 @@ class CanonicalUrlProcess extends AbstractCanonicalUrlProcess
                 $identifier = substr($identifier, 0, $suffixPosition);
             }
         }
-
-        return $identifier;
+        return rtrim($this->url->getBaseUrl(),  '/') . '/' . ltrim($identifier, '/');
     }
 }
